df5988cc1c9322da3d1d3121f8a519aedddfe106
Aleks <achudars@gmail.com>
2013-11-27 17:35:23 +0000

    basic MVC setup

--- config/includes.php
+++ config/includes.php
@@ -4,9 +4,13 @@
 require 'config/database.php';
 
 // MVC articles
-require 'controllers/articlesController.php';
+/*require 'controllers/articlesController.php';
 require 'models/articlesModel.php';
-require 'views/articlesView.php';
+require 'views/articlesView.php';*/
+
+require_once("controllers/controller.php");
+require_once("models/model.php");
+require_once("views/view.php");
 
 
  ?>
--- index.php
+++ index.php
@@ -1,47 +1,31 @@
 <?php
 
-require 'config/includes.php';
+require_once("config/includes.php");
 
-/* 	@IAPT Let's get our URL and find out to what page we should be directing */
 
-$page = basename($_SERVER['PHP_SELF']);
+$model = new Model();
+$controller = new Controller($model);
+$view = new View($controller, $model);
+echo $view->output();
 
-/* 	@IAPT If the page requested is not the index page, then we will need to do something */
+
+/*$page = basename($_SERVER['PHP_SELF']);
+
 if (!empty($page) && $page !== "index.php"){
-	echo "TEST";
-	/* 	@IAPT For each of the pages in our website, we create a view, a controller and a model.  When
-	*	someone requests the page, we instnce a version of each of those to run the page */
-	 if ($page == "articles")
-	 {
+	 if ($page == "articles") {
 
 	 	$model = new ArticleModel();
 	 	$controller = new ArticleController($model);
 	 	$view = new ArticleView($controller, $model);
 	 	echo $view->output();
 	 }
-	 /* @IAPT We do the same as we did for Article with the Videos link */
-/*	 else if ($page == "videos")
-	 {
-	 	$model = new VideosModel();
-	 	$controller = new VideosController($model);
-	 	$view = new VideosView($controller, $model);
-	 	echo $view->output();
-	 } */
-/*	 foreach($data as $key => $value){
-	 	if ($page == $key){
-	 		echo $page;
-	 		$m = $value['model'];
-	 		$v = $value['view'];
-	 		$c = $value['controller'];
-	 		break;
-	 	}
-	 }*/
+
 	}
 	else
 	{
 		include 'home.php';
 	}
-
+*/
 
 
 // foreach($data as $key => $value){
