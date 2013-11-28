3cc566af0fe68eb12ec579eb3fbdc9730e93af3b
Aleks <achudars@gmail.com>
2013-11-28 23:46:35 +0000

    article creation page added and styled

--- views/html/submit.html.php
+++ views/html/submit.html.php
@@ -1,94 +1,107 @@
 <h1>SUBMIT AN ARTICLE</h1>
 
 <div class="content">
-    <div class="block">
+    <div class="block article-editor">
 
-        <form action="add.php" method="post" autocomplete="off">
-            <input type="text" name="title" placeholder="Title" />
+        <form action="" method="post" autocomplete="off">
+            <input type="text" name="title" placeholder="Title" autofocus />
             <br />
             <br />
-            <textarea name="content" placeholder="Content"></textarea>
+            <textarea rows="20" name="content" placeholder="Content"></textarea>
             <br />
             <br />
+            <div class="content">
+            <span class="col">
 
-            <span class="left-col">
+            </span>
+            <span class="col">
                 <div>
-                    <input type = "radio" name = "user_roles" id = "subscriber" value = "subscriber" />
-                    <label for = "subscriber">Subscriber</label>
+                    <input checked type = "radio" name = "article_type" id = "basic_article" value = "basic_article" />
+                    <label for = "basic_article">Basic Article</label>
                 </div>
                 <div>
-                    <input type = "radio" name = "user_roles" id = "writer" value = "writer" />
-                    <label for = "writer">Writer</label>
+                    <input type = "radio" name = "article_type" id = "column_article" value = "column_article" />
+                    <label for = "column_article">Column Article</label>
                 </div>
                 <div>
-                    <input type = "radio" name = "user_roles" id = "editor" value = "editor" />
-                    <label for = "editor">Editor</label>
+                    <input type = "radio" name = "article_type" id = "review_article" value = "review_article" />
+                    <label for = "review_article">Review Article</label>
                 </div>
             </span>
-            <span class="mid-col">
+            <span class="col">
                 <div>
-                    <input type = "radio" name = "user_roles" id = "subscriber" value = "subscriber" />
-                    <label for = "subscriber">Subscriber</label>
+                    <input checked type = "radio" name = "columns" id = "column_1" value = "column_1" />
+                    <label for = "column_1">Column 1</label>
                 </div>
                 <div>
-                    <input type = "radio" name = "user_roles" id = "writer" value = "writer" />
-                    <label for = "writer">Writer</label>
+                    <input type = "radio" name = "columns" id = "column_2" value = "column_2" />
+                    <label for = "column_2">Column 2</label>
                 </div>
                 <div>
-                    <input type = "radio" name = "user_roles" id = "editor" value = "editor" />
-                    <label for = "editor">Editor</label>
+                    <input type = "radio" name = "columns" id = "column_3" value = "column_3" />
+                    <label for = "column_3">Column 3</label>
                 </div>
                 <div>
-                    <input type = "radio" name = "user_roles" id = "editor" value = "editor" />
-                    <label for = "editor">Editor</label>
-                </div>
-            </span>
-            <span class="right-col">
-                <div>
-                    <input type = "radio" name = "user_roles" id = "subscriber" value = "subscriber" />
-                    <label for = "subscriber">Subscriber</label>
-                </div>
-                <div>
-                    <input type = "radio" name = "user_roles" id = "writer" value = "writer" />
-                    <label for = "writer">Writer</label>
-                </div>
-                <div>
-                    <input type = "radio" name = "user_roles" id = "editor" value = "editor" />
-                    <label for = "editor">Editor</label>
-                </div>
-                <div>
-                    <input type = "radio" name = "user_roles" id = "writer" value = "writer" />
-                    <label for = "writer">Writer</label>
-                </div>
-                <div>
-                    <input type = "radio" name = "user_roles" id = "editor" value = "editor" />
-                    <label for = "editor">Editor</label>
+                    <input type = "radio" name = "columns" id = "column_4" value = "column_4" />
+                    <label for = "column_4">Column 4</label>
                 </div>
             </span>
+            <span class="col">
+                <div>
+                    <input checked type = "radio" name = "user_roles" id = "subscriber" value = "subscriber" />
+                    <label for = "subscriber">Submitted</label>
+                </div>
+                <div>
+                    <input type = "radio" name = "user_roles" id = "writer" value = "writer" />
+                    <label for = "writer">Under review</label>
+                </div>
+                <div>
+                    <input type = "radio" name = "user_roles" id = "editor" value = "editor" />
+                    <label for = "editor">Awaiting Changes</label>
+                </div>
+                <div>
+                    <input type = "radio" name = "user_roles" id = "writer" value = "writer" />
+                    <label for = "writer">Published</label>
+                </div>
+                <div>
+                    <input type = "radio" name = "user_roles" id = "editor" value = "editor" />
+                    <label for = "editor">Rejected</label>
+                </div>
+            </span>
+            <span class="col">
 
-            <div class="rating">
-                Points: 1
-                <input type="range" name="rating" min="1" max="5">5
-                <input type="rate">
+            </span>
             </div>
-
+            <br />
+            <br />
+            <div class="rating">
+                <div>Rating</div>
+                <input type="range" name="rating" min="1" max="5" />
+            </div>
+            <br />
             <div id="tags">
                 <span class="tag">PC</span>
                 <span class="tag">PS4</span>
                 <span class="tag">XBONE</span>
                 <input type="text" value="" placeholder="Add a tag" />
             </div>
-
+            <br />
             <div id="authors">
                 <span class="tag">ME</span>
                 <input type="text" value="" placeholder="Add an author" />
             </div>
+            <br />
 
-            <input type="checkbox" placeholder="mark as staff pick" />
+            <div>
+                <input type="checkbox" value="staff_pick" id="staff_pick" />
+                <label for = "staff_pick">Mark as Staff Pick</label>
+            </div>
+            <br />
 
             <input type="submit" value="SUBMIT" />
+
             <input type="submit" value="PUBLISH" />
-            <input type="submit" value="REJECT" />
+
         </form>
 
     </div>
