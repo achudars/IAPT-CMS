717eec09021fd8b33b6f2c384ee1cc33e3578c7a
Aleks <achudars@gmail.com>
2013-11-28 11:43:34 +0000

    added styled registration and login forms

--- views/html/register.html.php
+++ views/html/register.html.php
@@ -40,18 +40,24 @@ if (isset($_POST["username"], $_POST["password"])) {
 
 <h1>REGISTER</h1>
 
-<div class="content">
-    <div class="block">
+<div class="form-content">
 
+    <div class="left untouchable">
+        <div class="pull-right">
+            <p>Username</p>
+            <p>Password</p>
+        </div>
+    </div>
+    <div class="right">
         <?php if ( isset($error) ) { ?>
         <small style="color:#aa0000"><?php echo $error; ?></small>
         <?php } ?>
 
         <form action="index.php" method="post" autocomplete="off">
-            <input type="text" name="username" placeholder="Username" />
-            <input type="password" name="password" placeholder="Password" />
-            <input type="submit" value="Login" />
+            <input type="text" name="username" />
+            <input type="password" name="password" />
+            <input type="submit" value="Register" />
         </form>
-
     </div>
+
 </div>
