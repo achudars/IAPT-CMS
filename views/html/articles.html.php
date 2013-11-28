9073e7d87d33a588dc8d914b5f1b27e042a493c9
Aleks <achudars@gmail.com>
2013-11-28 01:03:23 +0000

    basic header styling added

--- views/html/articles.html.php
+++ views/html/articles.html.php
@@ -1,21 +1,21 @@
-<!-- @IAPT: A div that can be styled if desired for any introductory text.-->
-            <div id="intro">
-                <!-- @IAPT: This is our first heading in the main section - it is distinct from the top heading of the site, and
-                    will be encountered in order by machines that process headings on websites -->
-                <h1><?php echo $title; ?></h1>
-
-            </div>
-
-
-
-
-
-
-
-<?php foreach($articles as $article): ?>
+<div class="block">
+    <h1><?php echo $title; ?></h1>
+    <?php foreach($articles as $article): ?>
 
         <h3><?php echo $article->getTitle(); ?></h3>
-        <h4>Details:</h4>
+        <h4><?php echo $article->getContent(); ?></h4>
+        <h3><?php echo $article->getTimestamp(); ?></h3>
+        <h3><img src="<?php echo $article->getImage(); ?>"/></h3>
+        <h4><?php echo $article->getStatus(); ?></h4>
+        <h4><?php echo $article->getType(); ?></h4>
 
-<?php endforeach; ?>
+    <?php endforeach; ?>
+</div>
 
+<div class="block">
+    <h1><?php echo $title; ?></h1>
+</div>
+
+<div class="block">
+    <h1><?php echo $title; ?></h1>
+</div>
--- views/html/header.html.php
+++ views/html/header.html.php
@@ -1,32 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-        <title>IAPT CMS</title>
-        <meta name="description" content="">
-        <meta name="author" content="">
-
-        <!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
-        <link rel="shortcut icon" href="/favicon.ico">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-        <link rel="stylesheet" type="text/css" href="public/css/iapt.css" />
-    </head>
-
-    <body>
-        <main>
-            <header>
-
-            </header>
-            <nav>
-                <ul class="navlist">
-                    <li><a href="?page=articles">Home</a></li>
-                    <li><a href="?page=register">Register</a></li>
-                    <li><a href="?page=login">Login</a></li>
-                    <li><a href="?page=list">List</a></li>
-                    <li><a href="?page=submit">Submit</a></li>
-                    <li><a href="?page=check">Check</a></li>
-                    <li><a href="?page=admin">Admin</a></li>
-                </ul>
-            </nav>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- I am against ugly default fonts, hence resorting to Lato -->
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="public/css/style.css" />
+</head>
+<body>
+    <header>
+        <h1><span class="emphasis">C</span>MS</h1>
+        <input type="search" placeholder="Search" />
+    </header>
+    <nav>
+        <ul class="navlist">
+            <li class="active"><a href="?page=articles">Home</a></li>
+            <li><a href="?page=register">Register</a></li>
+            <li><a href="?page=login">Login</a></li>
+            <li><a href="?page=list">List</a></li>
+            <li><a href="?page=submit">Submit</a></li>
+            <li><a href="?page=check">Check</a></li>
+            <li><a href="?page=admin">Admin</a></li>
+        </ul>
+    </nav>
+    <main>
