c07a0e196964913f71f8a38b4ab100630621acde
Aleks <achudars@gmail.com>
2013-12-01 18:52:36 +0000

    edited html to support deletion and edit of articles
    as well as change the status of the article

--- views/html/articles.html.php
+++ views/html/articles.html.php
@@ -26,9 +26,29 @@
                         <option>Awaiting Changes</option>
                         <option>Submitted</option>
                     </select>
-                    <button>Edit</button>
-                    <button>Delete</button>
+
+
                 </span>
+                <form class="pull-right" action="?page=admin&action=change_article_status" method="post">
+                    <input type='hidden' name='article_id' value='<?php echo $article->getArticleId(); ?>' />
+                    <select onchange="this.form.submit();" name="article_status">
+                        <option <?php echo ($article->getArticleStatus()=='submitted')?'selected':'' ?> value="submitted" name='submitted'>submitted</option>
+                        <option <?php echo ($article->getArticleStatus()=='under_review')?'selected':'' ?> value="under_review" name='under_review'>under_review</option>
+                        <option <?php echo ($article->getArticleStatus()=='awaiting_changes')?'selected':'' ?> value="awaiting_changes" name='awaiting_changes'>awaiting_changes</option>
+                        <option <?php echo ($article->getArticleStatus()=='published')?'selected':'' ?> value="published" name='published'>published</option>
+                        <option <?php echo ($article->getArticleStatus()=='rejected')?'selected':'' ?> value="rejected" name='rejected'>rejected</option>
+                    </select>
+                </form>
+                <form class="pull-right" action="?page=admin&action=edit_article" method="post">
+                    <input type='hidden' name='action' value='delete' />
+                    <input type='hidden' name='article_id' value='<?php echo $article->getArticleId(); ?>' />
+                    <button onclick="this.form.submit(); return false;">edit</button>
+                </form>
+                <form class="pull-right" action="?page=admin&action=delete_article" method="post">
+                    <input type='hidden' name='action' value='delete' />
+                    <input type='hidden' name='article_id' value='<?php echo $article->getArticleId(); ?>' />
+                    <button onclick="this.form.submit(); return false;">delete</button>
+                </form>
 
             </li><?php endforeach; ?>
         </ol>
