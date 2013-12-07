cb0b207d66916658caebe59a2ac0d3ac0323c5a1
Aleks <achudars@gmail.com>
2013-12-07 07:58:51 +0000

    cleaned up DB files

--- config/connection.php
+++ config/connection.php
@@ -1,11 +1,7 @@
 <?php
-
     try {
-        //$pdo = new PDO("mysql:host=localhost;dbname=iaptdb", "root", "");
         $pdo = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
     } catch (PDOException $e) {
         exit("Database error.");
     }
-
-
 ?>
